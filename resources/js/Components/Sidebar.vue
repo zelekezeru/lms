bc2d1e2f8d15540ad6c51300bc4428edea797aac
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2025-03-04 01:19:49 -0800

    Update Sidebar.vue

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,178 +1,111 @@
-<script>
-import { Link } from '@inertiajs/vue3'; // Import the Link component
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
+<script setup>
+import { ref } from "vue";
+import { Link } from "@inertiajs/vue3";
+import DropdownLink from "@/Components/DropdownLink.vue";
 
-export default {
-    components: {
-        Link, // Register the Link component
-        Dropdown,
-        DropdownLink,
-    },
-    methods: {
-        // Add any methods you need here
-    }
-}
+const showingDropdowns = ref({
+    students: false,
+    courses: false,
+    teachers: false,
+    attendance: false,
+    exams: false,
+});
+
+const toggleDropdown = (key) => {
+    showingDropdowns.value[key] = !showingDropdowns.value[key];
+};
 </script>
 
 <template>
-    <!-- Sidebar -->
     <div class="sidebar" data-background-color="dark">
-        <div class="sidebar-logo">
-            <!-- Logo Header -->
-            <div class="logo-header" data-background-color="dark">
-                <Link :href="route('dashboard')" class="logo">
-                    <img src="/img/kaiadmin/logo_light.svg" alt="navbar brand" class="navbar-brand" height="20" />
-                </Link>
-                <div class="nav-toggle">
-                    <button class="btn btn-toggle toggle-sidebar">
-                        <i class="gg-menu-right"></i>
-                    </button>
-                    <button class="btn btn-toggle sidenav-toggler">
-                        <i class="gg-menu-left"></i>
-                    </button>
-                </div>
-                <button class="topbar-toggler more">
-                    <i class="gg-more-vertical-alt"></i>
-                </button>
-            </div>
-            <!-- End Logo Header -->
-        </div>
-        <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-wrapper">
             <div class="sidebar-content">
-                <ul class="nav nav-secondary">
-                    <!-- Dashboard Link -->
+                <div class="user">                    
+                    <div class="info">
+                        <a class="collapsed" data-toggle="collapse" href="#profileMenu">
+                            <span>
+                                Admin
+                                <span class="user-level">Administrator</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <ul class="nav">
                     <li class="nav-item active">
                         <Link :href="route('dashboard')">
                             <i class="fas fa-home"></i>
                             <p>Dashboard</p>
                         </Link>
                     </li>
-
-                    <!-- Section Title -->
                     <li class="nav-section">
-                        <span class="sidebar-mini-icon">
-                            <i class="fa fa-ellipsis-h"></i>
-                        </span>
                         <h4 class="text-section">College Management</h4>
                     </li>
-
-                    <!-- Students Management -->
+                    
+                    <!-- Students -->
                     <li class="nav-item">
-                        <a data-bs-toggle="collapse" href="#students">
+                        <a href="#" @click.prevent="toggleDropdown('students')">
                             <i class="fas fa-users"></i>
                             <p>Students</p>
                             <span class="caret"></span>
                         </a>
-                        <div class="collapse" id="students">
-                            <ul class="nav nav-collapse">
-                                <li>
-                                    <Link >
-                                        Add Student
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link >
-                                        Manage Students
-                                    </Link>
-                                </li>
-                            </ul>
+                        <div v-if="showingDropdowns.students" class="submenu">
+                            <DropdownLink>Add Student</DropdownLink>
+                            <DropdownLink>Manage Students</DropdownLink>
                         </div>
                     </li>
-
-                    <!-- Courses Management -->
+                    
+                    <!-- Courses -->
                     <li class="nav-item">
-                        <a data-bs-toggle="collapse" href="#courses">
+                        <a href="#" @click.prevent="toggleDropdown('courses')">
                             <i class="fas fa-book"></i>
                             <p>Courses</p>
                             <span class="caret"></span>
                         </a>
-                        <div class="collapse" id="courses">
-                            <ul class="nav nav-collapse">
-                                <li>
-                                    <Link >
-                                        Add Course
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link >
-                                        Manage Courses
-                                    </Link>
-                                </li>
-                            </ul>
+                        <div v-if="showingDropdowns.courses" class="submenu">
+                            <DropdownLink>Add Course</DropdownLink>
+                            <DropdownLink>Manage Courses</DropdownLink>
                         </div>
                     </li>
-
-                    <!-- Teachers Management -->
+                    
+                    <!-- Teachers -->
                     <li class="nav-item">
-                        <a data-bs-toggle="collapse" href="#teachers">
+                        <a href="#" @click.prevent="toggleDropdown('teachers')">
                             <i class="fas fa-chalkboard-teacher"></i>
                             <p>Teachers</p>
                             <span class="caret"></span>
                         </a>
-                        <div class="collapse" id="teachers">
-                            <ul class="nav nav-collapse">
-                                <li>
-                                    <Link >
-                                        Add Teacher
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link >
-                                        Manage Teachers
-                                    </Link>
-                                </li>
-                            </ul>
+                        <div v-if="showingDropdowns.teachers" class="submenu">
+                            <DropdownLink>Add Teacher</DropdownLink>
+                            <DropdownLink>Manage Teachers</DropdownLink>
                         </div>
                     </li>
-
-                    <!-- Attendance Management -->
+                    
+                    <!-- Attendance -->
                     <li class="nav-item">
-                        <a data-bs-toggle="collapse" href="#attendance">
+                        <a href="#" @click.prevent="toggleDropdown('attendance')">
                             <i class="fas fa-calendar-check"></i>
                             <p>Attendance</p>
                             <span class="caret"></span>
                         </a>
-                        <div class="collapse" id="attendance">
-                            <ul class="nav nav-collapse">
-                                <li>
-                                    <Link >
-                                        Take Attendance
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link >
-                                        View Attendance
-                                    </Link>
-                                </li>
-                            </ul>
+                        <div v-if="showingDropdowns.attendance" class="submenu">
+                            <DropdownLink>Take Attendance</DropdownLink>
+                            <DropdownLink>View Attendance</DropdownLink>
                         </div>
                     </li>
-
-                    <!-- Exams Management -->
+                    
+                    <!-- Exams -->
                     <li class="nav-item">
-                        <a data-bs-toggle="collapse" href="#exams">
+                        <a href="#" @click.prevent="toggleDropdown('exams')">
                             <i class="fas fa-clipboard-list"></i>
                             <p>Exams</p>
                             <span class="caret"></span>
                         </a>
-                        <div class="collapse" id="exams">
-                            <ul class="nav nav-collapse">
-                                <li>
-                                    <Link >
-                                        Schedule Exam
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link >
-                                        Manage Exams
-                                    </Link>
-                                </li>
-                            </ul>
+                        <div v-if="showingDropdowns.exams" class="submenu">
+                            <DropdownLink>Schedule Exam</DropdownLink>
+                            <DropdownLink>Manage Exams</DropdownLink>
                         </div>
                     </li>
-
-                    <!-- Profile Link -->
+                    
                     <li class="nav-item">
                         <Link :href="route('profile.edit')">
                             <i class="fas fa-user"></i>
@@ -183,5 +116,6 @@ export default {
             </div>
         </div>
     </div>
-    <!-- End Sidebar -->
 </template>
+
+

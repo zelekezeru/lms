7267339ba1813853d7492e3d2f3920aa5c1d5f0c
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2025-03-04 22:57:25 -0800

    Update Edit.vue

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -20,12 +20,12 @@ defineProps({
 
     <AppLayout>
         <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            <h2 class="flex flex-col text-xl font-semibold leading-tight text-gray-800">
                 Profile
             </h2>
         </template>
         
-        <div class="py-12">
+        <div class="py-12 flex flex-col">
             <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
                 <div
                     class="bg-white p-4 shadow sm:rounded-lg sm:p-8"

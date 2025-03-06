89588cb77313debb5b436bfd0ab1e3fd70bcddd2
Abel Shiferaw <109241143+ShiferawAbel@users.noreply.github.com>
2025-03-06 11:09:09 -0800

    disabled data wrapping for resources

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        JsonResource::withoutWrapping();
         Vite::prefetch(concurrency: 3);
     }
 }

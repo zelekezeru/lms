39b1e88b480fd37c151944fc637cf602dde3811d
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2025-03-14 15:21:27 -0700

    Update GuestLayout.vue

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -7,7 +7,7 @@ import { Link } from '@inertiajs/vue3';
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
     >
-        <div>zz
+        <div>
             <Link href="/">
                 <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
             </Link>

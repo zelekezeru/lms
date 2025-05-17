c4c34304c541689ac2e9063a3bc3f937c8f9f0d0
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2025-05-17 05:20:49 +0300

    build May 17 2

---
+++ public/build/assets/InputLabel-ByZw8d9N.js
@@ -0,0 +1,1 @@
+import{c as t,o as e,t as o,m as n}from"./app-DWmz0_yw.js";const l={class:"block text-sm font-medium text-gray-700 dark:text-gray-200"},r={key:0},c={key:1},m={__name:"InputLabel",props:{value:{type:String}},setup(a){return(s,_)=>(e(),t("label",l,[a.value?(e(),t("span",r,o(a.value),1)):(e(),t("span",c,[n(s.$slots,"default")]))]))}};export{m as _};

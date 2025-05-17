ccb53ce6f261d1493aac2bfe48d4a650f1870869
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2025-05-17 12:33:29 +0300

    Build May 17 3

---
+++ public/build/assets/Index-D2uARPJB.js
@@ -0,0 +1,1 @@
+import{c as t,o as a,a as n}from"./app-DtcGZuoJ.js";const l={__name:"Index",setup(o){return(r,e)=>(a(),t("div",null,e[0]||(e[0]=[n("h1",null,"Schedules",-1)])))}};export{l as default};

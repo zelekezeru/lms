b5406d8e63d7bbc2c3503bf80aac530defb67804
Abel Shiferaw <109241143+ShiferawAbel@users.noreply.github.com>
2025-06-02 00:17:22 +0300

    build on june 2
    - this deployment requires migrate fresh and seed

--- public/build/assets/InputError-BjHcO06r.js
+++
@@ -1,1 +0,0 @@
-import{m as s,B as t,c as a,o as r,a as o,t as c}from"./app-C4B5X69o.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((r(),a("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};

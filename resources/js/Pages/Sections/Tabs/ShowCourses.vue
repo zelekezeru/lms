ef9923390803d44cf6a49544bd7520d1e27a882b
Abel Shiferaw <109241143+ShiferawAbel@users.noreply.github.com>
2025-06-05 01:43:08 +0300

    bug fixed and build on june 5 4

--- resources/js/Pages/Sections/Tabs/ShowCourses.vue
+++ resources/js/Pages/Sections/Tabs/ShowCourses.vue
@@ -250,7 +250,7 @@ function submitMove() {
                     class="block w-full px-3 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                 >
                     <option
-                        v-for="i in section.track.duration"
+                        v-for="i in parseInt(section.track.duration)"
                         :key="i"
                         :value="i"
                     >
@@ -272,11 +272,7 @@ function submitMove() {
                     v-model="selectedSemester"
                     class="block w-full px-3 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                 >
-                    <option
-                        v-for="i in section.track.duration"
-                        :key="i"
-                        :value="i"
-                    >
+                    <option v-for="i in 3" :key="i" :value="i">
                         Semester {{ i }}
                     </option>
                 </select>

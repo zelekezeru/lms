eca8933998eb7a143fc5ef1284b2b758adcc0838
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2025-11-09 23:28:47 +0300

    Update StudentsImport.php

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -125,8 +125,10 @@ class StudentsImport implements ToCollection, WithHeadingRow
                 $student = Student::updateOrCreate(
                     [
                         'user_id' => $user->id,
-                        'id_no' => $userUuid,
+                        'year_id' => $year->id,
+                        'mobile_phone' => $phone,
                     ],
+                    
                     $this->prepareStudentData($row, $firstName, $middleName, $lastName, $userUuid, $year, $semester, $user->id, $phone, $section->id)
                 );
 

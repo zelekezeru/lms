797603df8197f458181133f492652741245cfa0c
Abel Shiferaw <109241143+ShiferawAbel@users.noreply.github.com>
2025-12-26 18:39:08 +0300

    build on dec 26

---
+++ public/build/assets/StudentsHome-0tKG5A5h.js
@@ -0,0 +1,1 @@
+import{b as t,o,a}from"./app-Dy-NPSHL.js";/* empty css            */const m={__name:"StudentsHome",setup(n){return(r,e)=>(o(),t("div",null,e[0]||(e[0]=[a("h1",null,"Welcome to the Students Home Page",-1)])))}};export{m as default};
